1fd583a35f8177179c4bab02a3ebd6f6c5de3899
harshvardhan.sharma <harshvardhan.sharma@HARSH-SHARMA>
2018-01-12 13:13:02 +0530

    Removing EF6 Repository

--- Framework/NS.Framework.Repository.Pattern.EFCore2/IDbSet.cs
+++
@@ -1,8 +0,0 @@
-﻿using NS.Framework.Repository.Pattern.EFCore2;
-
-namespace NS.Frameowrk.Repository.Pattern.EFCore2
-{
-    internal interface IDbSet<TEntity> where TEntity : Entity, new()
-    {
-    }
-}
